6f765b722494a12aa1474946481c751c48ac8e74
Batuhan SAHIN <baturox@gmail.com>
2022-11-24 15:30:54 +0300

    event counter added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,34 +6,63 @@ export default function HomePage() {
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
   const [z, setZ] = useState(0);
+  const [eventCount, setEventCount] = useState(0);
 
   useEffect(() => {
-    window.addEventListener("deviceorientation", (event) => {
-      setX(event.alpha);
-      setY(event.beta);
-      setZ(event.gamma);
-    });
-  }, []);
+    if(permission && isStarting){
+      window.addEventListener("deviceorientation", (event) => {
+        setX(event.alpha);
+        setY(event.beta);
+        setZ(event.gamma);
+        setEventCount(eventCount => eventCount+1);
+      });
+    }else{
+      window.removeEventListener("deviceorientation", (event) => {
+        setX(event.alpha);
+        setY(event.beta);
+        setZ(event.gamma);
+        setEventCount(0);
+      });
+    }
+  }, [permission, isStarting]);
+
+  const getPermission = () => {
+    if (
+      typeof DeviceMotionEvent !== 'undefined' &&
+      DeviceMotionEvent &&
+      typeof DeviceMotionEvent.requestPermission === "function"
+    ) {
+      DeviceMotionEvent.requestPermission().then(response => {
+        if (response == "granted") {
+          setPermission(true);
+        }
+      })
+    } else {
+      navigator.permissions.query({ name: "gyroscope" })
+        .then(result => {
+          if (result.state === "granted") {
+            setPermission(true);
+          }
+        });
+    }
+  }
 
   return (<>
     <button onClick={() => {
-      setIsStarting(true);
-      if (
-        DeviceMotionEvent &&
-        typeof DeviceMotionEvent.requestPermission === "function"
-      ) {
-        DeviceMotionEvent.requestPermission().then(response => {
-          if (response == "granted") {
-            setPermission(true);
-          }
-        })
+      if(isStarting){
+        setIsStarting(false);
+      }else{
+        setIsStarting(true);
+        getPermission();
       }
-    }}>{'Start'}</button>
+    
+    }}>{!isStarting ? 'Start' : 'Stop'}</button>
 
-    {(isStarting) && (<ul>
-      <li>X: {x}</li>
-      <li>Y: {y}</li>
-      <li>Z: {z}</li>
+    {(isStarting && permission) && (<ul>
+      <li>Event Count: {eventCount}</li>
+      <li>X: {Number(x).toFixed(2)}</li>
+      <li>Y: {Number(y).toFixed(2)}</li>
+      <li>Z: {Number(z).toFixed(2)}</li>
     </ul>)}
   </>);
 }
