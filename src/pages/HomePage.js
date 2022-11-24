023568a3afe5d9d17141bf91cdbf262d5470e287
Batuhan SAHIN <baturox@gmail.com>
2022-11-24 15:09:50 +0300

    permission update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,25 +1,38 @@
 import React, { useEffect, useState } from 'react';
-import useNavigatorPermissions from 'react-use-navigator-permissions'
 
 export default function HomePage() {
+  const [permission, setPermission] = useState(false);
+  const [isStarting, setIsStarting] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
   const [z, setZ] = useState(0);
-  const { status, error } = useNavigatorPermissions('gyroscope')
 
   useEffect(() => {
-    console.log(status);
-    if (status && status == 'granted') {
+    if (permission) {
       window.addEventListener("deviceorientation", (event) => {
         setX(event.alpha);
         setY(event.beta);
         setZ(event.gamma);
       });
     }
-  }, [status]);
+  }, [permission]);
 
   return (<>
-    {(<ul>
+    <button onClick={() => {
+      setIsStarting(true);
+      if (
+        window.DeviceMotionEvent &&
+        typeof window.DeviceMotionEvent.requestPermission === "function"
+      ) {
+        window.DeviceMotionEvent.requestPermission().then(response => {
+          if (response == "granted") {
+            setPermission(true);
+          }
+        })
+      }
+    }}>{'Start'}</button>
+
+    {(isStarting  && permission) && (<ul>
       <li>X: {x}</li>
       <li>Y: {y}</li>
       <li>Z: {z}</li>
