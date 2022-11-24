75343e2f6a4ecad4098f3149b6f23104cb0fbc7a
Batuhan SAHIN <baturox@gmail.com>
2022-11-24 16:27:38 +0300

    height cahnge

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -61,7 +61,7 @@ export default function HomePage() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const geometry = new THREE.BoxGeometry(1, 1, 2);
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     cube = (new THREE.Mesh(geometry, material));
     scene.add(cube);
