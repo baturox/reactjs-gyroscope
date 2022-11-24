04d7618139ff9e1020f373c0d760d3a63826c930
Batuhan SAHIN <baturox@gmail.com>
2022-11-24 14:46:48 +0300

    hot fix

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,13 +6,12 @@ export default function HomePage() {
   const [y, setY] = useState(0);
   const [z, setZ] = useState(0);
 
-  const handlePermission = () => {
-    Promise.all([navigator.permissions.query({ name: "accelerometer" }),
-    navigator.permissions.query({ name: "magnetometer" }),
-    navigator.permissions.query({ name: "gyroscope" })])
-      .then(results => {
-        if (results.every(result => result.state === "granted")) {
-          setPermission(true);
+  useEffect(() => {
+    navigator.permissions.query({ name: "gyroscope" })
+      .then(result => {
+        console.log(result.state);
+        if (result.state === "granted") {
+          // setPermission(true);
           window.addEventListener("deviceorientation", (event) => {
             setX(event.alpha);
             setY(event.beta);
@@ -22,14 +21,11 @@ export default function HomePage() {
           console.log("No permissions to use AbsoluteOrientationSensor.");
         }
       });
-  }
-
-  useEffect(() => {
-    handlePermission();
   }, []);
 
   return (<>
-    {permission && (<ul>
+    {permission}
+    {(<ul>
       <li>X: {x}</li>
       <li>Y: {y}</li>
       <li>Z: {z}</li>
