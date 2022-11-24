038915337c5392d7ad66bbb703229c2dba28f67b
Batuhan SAHIN <baturox@gmail.com>
2022-11-24 14:26:05 +0300

    permission check

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,14 +6,26 @@ export default function HomePage() {
   const [y, setY] = useState(0);
   const [z, setZ] = useState(0);
 
+  const handlePermission = () => {
+    Promise.all([navigator.permissions.query({ name: "accelerometer" }),
+    navigator.permissions.query({ name: "magnetometer" }),
+    navigator.permissions.query({ name: "gyroscope" })])
+      .then(results => {
+        if (results.every(result => result.state === "granted")) {
+          window.addEventListener("deviceorientation", (event) => {
+            setX(event.alpha);
+            setY(event.beta);
+            setZ(event.gamma);
+          });
+        } else {
+          console.log("No permissions to use AbsoluteOrientationSensor.");
+        }
+      });
+  }
 
   useEffect(() => {
-    window.addEventListener("deviceorientation", (event) => {
-      setX(event.alpha);
-      setY(event.beta);
-      setZ(event.gamma);
-    });
-  });
+    handlePermission();
+  }, []);
 
   return (<>
     <ul>
