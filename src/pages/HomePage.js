e37e248cde64a0b6adac4027af8f33914252ca80
Batuhan SAHIN <baturox@gmail.com>
2022-11-24 14:27:08 +0300

    permission check return

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 export default function HomePage() {
-
+  const [permission, setPermission] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
   const [z, setZ] = useState(0);
@@ -12,6 +12,7 @@ export default function HomePage() {
     navigator.permissions.query({ name: "gyroscope" })])
       .then(results => {
         if (results.every(result => result.state === "granted")) {
+          setPermission(true);
           window.addEventListener("deviceorientation", (event) => {
             setX(event.alpha);
             setY(event.beta);
@@ -28,10 +29,10 @@ export default function HomePage() {
   }, []);
 
   return (<>
-    <ul>
+    {permission && (<ul>
       <li>X: {x}</li>
       <li>Y: {y}</li>
       <li>Z: {z}</li>
-    </ul>
+    </ul>)}
   </>);
 }
